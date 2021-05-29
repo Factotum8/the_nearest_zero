0b6bb2f608cd88192d6cff1d10951a691528cc1f
Alexandr Amochkin <a.amochkin@latera.ru>
2021-05-30 01:07:08 +0300

    README.md is edited

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-def calculate_distances(count: int, cells: list[int]):
+def calculate_distances(count: int, cells: list[int]) -> list[int]:
+    """
+    * Заполняем пустой лист result_distances бесконечностями float('inf'). Длина листа как у "улицы".
+    * С перового нуля проходим по "улице" в конец, вычисляя расстояния до участков. Записываем в лист result_distances
+        min(текущая дистанция, result_distances[текущий индекс]).
+    * С последнего нуля идём в начало "улицы" беря min(текущая дистанция, result_distances[текущий индекс]).
+    """
+    def iterate_over(iterator):
+        distance, zero_indx = None, None
+        for i in iterator:
+            if cells[i] == 0:
+                distance = 0
+                zero_indx = i
+            result_distances[i] = min(result_distances[i], distance)
+            distance += 1
+        return zero_indx
+
     result_distances = [float('inf')] * count
-    distance, last_zero_indx = None, None
-
-    # def iterate_over(iterator):
-    #     for i in iterator:
-    #         if cells[i] == 0:
-    #             distance = 0
-    #             last_zero_indx = i
-    #         result_distances[i] = min(result_distances[i], distance)
-    #         distance += 1
-    #     return last_zero_indx
-    #
-    # r = iterate_over(range(cells.index(0), count))
-    # iterate_over(reversed(range(r)))
-
-    for i in range(cells.index(0), count):
-        if cells[i] == 0:
-            distance = 0
-            last_zero_indx = i
-        result_distances[i] = min(result_distances[i], distance)
-        distance += 1
-
-    for i in reversed(range(last_zero_indx)):
-        if cells[i] == 0:
-            distance = 0
-        result_distances[i] = min(result_distances[i], distance)
-        distance += 1
+    last_zero_index = iterate_over(range(cells.index(0), count))
+    iterate_over(reversed(range(last_zero_index + 1)))
 
     return result_distances
 
@@ -37,7 +29,7 @@ def main():
     except ValueError as e:
         print(f"wrong input: {e}")
         return
-    calculate_distances(cells_count, cells)
+    print(calculate_distances(cells_count, cells))
 
 
 if __name__ == '__main__':
