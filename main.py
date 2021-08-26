f01d6bf1b2df532f00403fd18cc599f908625a00
Alexandr Amochkin <a.amochkin@latera.ru>
2021-08-26 11:36:33 +0300

    Refactoring naming

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ def calculate_distances(count: int, cells: list[int]) -> list[int]:
     """
     * Заполняем пустой лист result_distances бесконечностями float('inf'). Длина листа как у "улицы".
     * С перового нуля проходим по "улице" в конец, вычисляя расстояния до участков. Записываем в лист result_distances
-        min(текущая дистанция, result_distances[текущий индекс]).
-    * С последнего нуля идём в начало "улицы" беря min(текущая дистанция, result_distances[текущий индекс]).
+        min(текущая дистанция до нуля, result_distances[текущий индекс]).
+    * С последнего нуля идём в начало "улицы" беря min(текущая дистанция до нуля, result_distances[текущий индекс]).
     """
+    nearest_symbol = 0
+
     def iterate_over(iterator):
-        distance, zero_indx = None, None
+        distance, symbol_indx = None, None
         for i in iterator:
-            if cells[i] == 0:
+            if cells[i] == nearest_symbol:
                 distance = 0
-                zero_indx = i
+                symbol_indx = i
             result_distances[i] = min(result_distances[i], distance)
             distance += 1
-        return zero_indx
+        return symbol_indx
 
     result_distances = [float('inf')] * count
-    last_zero_index = iterate_over(range(cells.index(0), count))
-    iterate_over(reversed(range(last_zero_index + 1)))
+    last_symbol_index = iterate_over(range(cells.index(nearest_symbol), count))
+    iterate_over(reversed(range(last_symbol_index + 1)))
 
     return result_distances
 
